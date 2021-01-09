817fb504f9f4a6dbdd98f98555a95dad663f4c46
Mike <michael.hallam3@gmail.com>
2021-01-09 15:15:59 +0000

    wrote a basic routine to generate rooms for people to match movies in

---
+++ app/models/room/RoomGen.scala
@@ -0,0 +1,38 @@
+package room
+
+trait RoomGen {
+  def genRoomCode: String
+}
+
+object RoomGenFact {
+  def getRoomGen: RoomGen = RoomGenImpl
+}
+
+private object RoomGenImpl extends RoomGen {
+  private var occupied: List[Room] = Nil
+
+  override def genRoomCode: String = {
+    var curRoom: Room = null
+    while(null != curRoom && !occupied.exists(_==curRoom))
+      curRoom = genRoom
+    addRoom(curRoom)
+    curRoom.roomID
+  }
+
+  private def addRoom(x: Room): Unit = 
+    occupied = x::occupied
+
+  private def removeRoom(x: Room) = 
+    occupied = occupied.filterNot(_==x)
+  
+  private def genRoom: Room = {
+    Room(genRandCharString)
+  }
+
+  // TODO - make this generate an actual random string of characters
+  private def genRandCharString: String =
+    "placeholder"
+}
+
+private case class Room(roomID: String)
+
